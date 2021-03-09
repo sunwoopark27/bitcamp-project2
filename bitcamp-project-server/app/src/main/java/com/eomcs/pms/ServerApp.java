f92c2246ef552c3a655920391a7bdccc29696f52
sunwoopark27 <sun960127@naver.com>
2021-03-09 22:48:35 +0900

    .

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -4,10 +4,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import com.eomcs.pms.table.BoardTable;
+import com.eomcs.pms.table.DataTable;
 
 //데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
 public class ServerApp {
   int port;
+  HashMap<String,DataTable> tableMap = new HashMap<>();
+
   public static void main(String[] args) {
     ServerApp app = new ServerApp(8888);
     app.service();
@@ -16,6 +23,10 @@ public class ServerApp {
     this.port = port;
   }
   public void service() {
+
+    // 요청을 처리할 테이블 객체 준비
+    tableMap.put("board/", new BoardTable());
+
     // 클라이언트 연결을 기다리는 서버 소켓 생성
     try(ServerSocket serverSocket = new ServerSocket(this.port)){
       System.out.println("서버 실행!");
@@ -35,15 +46,58 @@ public class ServerApp {
         DataInputStream in = new DataInputStream(socket.getInputStream())) {
 
       while(true) {
+        // 1) 명령어 문자열을 읽는다.
         String request = in.readUTF();
-        System.out.println(request);
+        // 2) 클라이언트가 보낸 데이터의 개수를 읽는다.
+        int length = in.readInt();
 
-        out.writeUTF("success");
-        out.flush();
+        // 3) 클라이언트가 보낸 데이터를 읽어서 List 컬렉션에 담는다.
+        ArrayList<String> data = null;
+        if(length > 0) {
+          data = new ArrayList<>();
+          for(int i = 0; i < length; i++) {
+            data.add(in.readUTF());
+          }
+        }
+
+        System.out.println("----------------------------------");
+        System.out.printf("명령: %s",request);
+        System.out.printf("데이터 개수: %d", length);
+        if(data != null) {
+          System.out.println("데이터: ");
+          for (String str : data) {
+            System.out.println(str);
+          }
+        }
 
         if (request.equals("quit")) {
+          out.writeUTF("success");
+          out.writeInt(0);
+          out.flush(); // 정확하게
           break;
         }
+
+        DataTable dataTable = null;
+
+        Set<String> keySet = tableMap.keySet();
+        for (String key : keySet) {
+          if (request.startsWith(key)) {
+            dataTable = tableMap.get(key);
+            break;
+          }
+        }
+
+        if(dataTable != null) {
+          dataTable.service(request, data, out);
+        } else {
+          out.writeUTF("error");
+          out.writeInt(1);
+          out.writeUTF("해당 요청을 처리할 수 없습니다.");
+        }
+
+        out.flush(); //성공했든 안했든 flush
+
+
       }
     } catch (Exception e) {
       System.out.println("클라이언트 요청을 처리하는 중에 오류 발생!" );
---
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/table/BoardTable.java
@@ -0,0 +1,108 @@
+package com.eomcs.pms.table;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.sql.Date;
+import java.util.List;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.JsonFileHandler;
+
+public class BoardTable implements DataTable {
+
+  File jsonFile = new File("boards.json");
+  List<Board> list;
+
+  public BoardTable() {
+    list = JsonFileHandler.loadObjects(jsonFile, Board.class);
+  }
+  @Override
+  public void service(String command, List<String> data, DataOutputStream out) throws Exception {
+    Board board = null;
+    switch(command) {
+      case "board/insert" : 
+
+        // data에서 CSV 형식으로 표현된 문자열을 꺼내 각 필드 값으로 분리한다.
+        String[] fields = data.get(0).split(",");
+
+        // CSV 데이터를 Board 객체에 저장ㅎ나다.
+        board = new Board();
+
+        // 새 게시글의 번호
+        if(list.size() > 0) {
+          board.setNo(list.get(list.size() - 1).getNo() + 1);
+        } else {
+          board.setNo(1);
+        }
+
+        board.setTitle(fields[0]);
+        board.setContent(fields[1]);
+        board.setWriter(fields[2]);
+        board.setRegisteredDate(new Date(System.currentTimeMillis()));
+
+        // 새 게시글을 담은 Board 객체를 목록에 저장한다
+        list.add(board);
+
+        // 클라이언트에게 작업 성공을 알린다,
+        out.writeUTF("success");
+        out.writeInt(0);
+        break;
+
+      case "board/selectall" :
+
+        out.writeUTF("success");
+        out.writeInt(list.size());
+        for(Board b : list) {
+          out.writeUTF(String.format("%d, %s, %s, %s, %d",
+              b.getNo(), b.getTitle(), b.getWriter(), b.getRegisteredDate(), b.getViewCount()));
+        }
+        break;
+
+      case "board/select" :
+        int no = Integer.parseInt(data.get(0));// 데이터 한개 꺼내라 그게 바로 게시글 번호
+
+        board = getBoard(no);
+        if (board != null) { //해당 게시물 찾았다느거
+          //출력
+          out.writeUTF("success");
+          out.writeInt(1);
+          out.writeUTF(String.format("%d, %s, %s, %s, %s, %d",
+              board.getNo(),
+              board.getTitle(),
+              board.getContent(),
+              board.getWriter(),
+              board.getRegisteredDate(),
+              board.getViewCount()));
+        } else {
+          out.writeUTF("error");
+          out.writeInt(1);
+          out.writeUTF("해당 번호의 게시글이 없습니다.");
+        }
+
+
+        break;
+      case "board/update" :
+        out.writeUTF("success");
+        out.writeInt(0);
+        break;
+      case "board/delete" :
+        out.writeUTF("success");
+        out.writeInt(0);
+        break;
+      default:
+        out.writeUTF("error");
+        out.writeInt(1);
+        out.writeUTF("해당 명령을 처리할 수 없습니다.");
+
+    }
+    //    out.flush(); // App에서 flush 함
+  }
+
+  private Board getBoard(int boardNo) {
+    for (Board b : list) {
+      if(b.getNo() == boardNo) {
+        return b;
+      }
+    }
+    return null;
+  }
+}
