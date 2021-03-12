4ad76d75666eeb71694f430c81e66eabd2a03b5a
sunwoopark27 <sun960127@naver.com>
2021-03-12 10:07:39 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -12,6 +12,11 @@ import com.eomcs.pms.handler.BoardListHandler;
 import com.eomcs.pms.handler.BoardSearchHandler;
 import com.eomcs.pms.handler.BoardUpdateHandler;
 import com.eomcs.pms.handler.Command;
+import com.eomcs.pms.handler.MemberAddHandler;
+import com.eomcs.pms.handler.MemberDeleteHandler;
+import com.eomcs.pms.handler.MemberDetailHandler;
+import com.eomcs.pms.handler.MemberListHandler;
+import com.eomcs.pms.handler.MemberUpdateHandler;
 import com.eomcs.util.Prompt;
 
 public class ClientApp {
@@ -44,13 +49,13 @@ public class ClientApp {
     commandMap.put("/board/update", new BoardUpdateHandler());
     commandMap.put("/board/delete", new BoardDeleteHandler());
     commandMap.put("/board/search", new BoardSearchHandler());
-    //
-    //    commandMap.put("/member/add", new MemberAddHandler());
-    //    commandMap.put("/member/list", new MemberListHandler());
-    //    commandMap.put("/member/detail", new MemberDetailHandler());
-    //    commandMap.put("/member/update", new MemberUpdateHandler());
-    //    commandMap.put("/member/delete", new MemberDeleteHandler());
-    //
+
+    commandMap.put("/member/add", new MemberAddHandler());
+    commandMap.put("/member/list", new MemberListHandler());
+    commandMap.put("/member/detail", new MemberDetailHandler());
+    commandMap.put("/member/update", new MemberUpdateHandler());
+    commandMap.put("/member/delete", new MemberDeleteHandler());
+
     //    commandMap.put("/project/add", new ProjectAddHandler());
     //    commandMap.put("/project/list", new ProjectListHandler());
     //    commandMap.put("/project/detail", new ProjectDetailHandler());
