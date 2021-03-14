9087e8be09726d39fbff4a43a066426282ca0205
sunwoopark27 <sun960127@naver.com>
2021-03-14 23:55:14 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -3,6 +3,7 @@ package com.eomcs.pms;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import com.eomcs.util.Prompt;
 
 public class ClientApp {
@@ -27,11 +28,44 @@ public class ClientApp {
       while(true) {
         String message = Prompt.inputString("명령> ");
 
+        // 1) 명령어를 보낸다.
         out.writeUTF(message);
+
+        // 2) 서버에 보낼 데이터의 개수를 보낸다.
+        out.writeInt(3);
+
+        // 3) 서버에 데이터를 보낸다.
+        out.writeUTF("aaa");
+        out.writeUTF("bbb");
+        out.writeUTF("ccc");
+
         out.flush();
 
+        // 서버가 보낸 데이터를 읽는다.
+        // 1) 작업 결과를 읽는다.
         String response = in.readUTF();
-        System.out.println(response);
+
+        // 2) 데이터의 개수를 읽는다
+        int length = in.readInt();
+
+        // 3) 데이터의 개수만큼 읽어 List 컬렉션에 보관한다.
+        ArrayList<String> data = null;
+        if(length > 0) {
+          data = new ArrayList<>();
+          for(int i = 0; i < length; i++) {
+            data.add(in.readUTF());
+          }
+        }
+
+        System.out.println("----------------------------");
+        System.out.printf("작업 결과: %s\n", response);
+        System.out.printf("데이터 개수: %d\n", length);
+        if (data != null) {
+          System.out.println("데이터:");
+          for (String str : data) {
+            System.out.println(str);
+          }
+        }
 
         if(message.equals("quit")) {
           break;
--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 // 데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
 public class ServerApp {
@@ -37,11 +38,40 @@ public class ServerApp {
         DataInputStream in = new DataInputStream(socket.getInputStream());){
 
       while (true) {
+        // 1) 명령어 문자열을 읽는다.
         String request = in.readUTF();
-        System.out.println(request);
-        out.flush();
 
+        // 2) 클라이언트가 보낸 데이터의 개수를 읽는다.
+        int length = in.readInt();
+
+        // 3) 클라이언트가 보낸 데이터를 읽어서 List에 담는다
+        ArrayList<String> data = null;
+        if(length > 0) {
+          data = new ArrayList<>();
+          for (int i = 0; i < length; i++) {
+            data.add(in.readUTF());
+          }
+        }
+
+        System.out.println("-------------------------------");
+        System.out.printf("명령 : %s\n", request);
+        System.out.printf("데이터 개수 : %d\n", length);
+        if(data != null) {
+          System.out.println("데이터 : ");
+          for(String str : data) {
+            System.out.println(str);
+          }
+        }
+
+        // 1) 클라이언트에게 요청에 대한 작업 결과를 보낸다.
         out.writeUTF("success");
+
+        // 2) 클라이언트에게 보낼 데이터의 개수를 보낸다.
+        out.writeInt(1);
+
+        // 3) 클라이언트에게 데이터를 보낸다
+        out.writeUTF("test...ok");
+
         out.flush();
 
         if(request.equals("quit")) {
