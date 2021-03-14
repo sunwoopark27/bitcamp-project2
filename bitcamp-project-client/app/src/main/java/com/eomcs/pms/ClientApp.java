66056c0282e1e25ff744c56f49e3e9243e45bf69
sunwoopark27 <sun960127@naver.com>
2021-03-14 22:56:04 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -1,7 +1,23 @@
 package com.eomcs.pms;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClientApp {
   public static void main(String[] args) {
-    System.out.println("미니 프로젝트 관리 시스템");
+    try(Socket socket = new Socket("localhost", 8888);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+
+      out.writeUTF("hello");
+      out.flush();
+
+      String response = in.readUTF();
+      System.out.println(response);
+
+    }catch (Exception e) {
+      System.out.println("서버와 통신하는 중 오류 발생!");
+    }
   }
 }
--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -1,8 +1,52 @@
 package com.eomcs.pms;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // 데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
 public class ServerApp {
+
+  int port;
+
   public static void main(String[] args) {
-    System.out.println("파일 관리 서버");
+    ServerApp app = new ServerApp(8888);
+    app.service();
   }
+
+  public ServerApp(int port) {
+    this.port = port;
+  }
+
+  private void service() {
+
+    try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+      System.out.println("서버 실행!");
+
+      processRequest(serverSocket.accept());
+
+    } catch(Exception e) {
+      System.out.println("서버 실행 중 오류 발생!");
+      e.printStackTrace();
+    }
+  }
+
+  private void processRequest(Socket socket) {
+    try(DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());){
+
+      String request = in.readUTF();
+      System.out.println(request);
+      out.flush();
+
+      out.writeUTF("success");
+      out.flush();
+    } catch (Exception e) {
+      System.out.println("클라이언트의 요청을 처리하는 중 오류 발생!");
+      e.printStackTrace();
+    }
+
+  }
+
 }
