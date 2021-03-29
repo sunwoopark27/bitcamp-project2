305ee267c1291523f432276dd444593f9091d80d
박선우 <sun960127@naver.com>
2021-03-29 09:50:04 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskDetailHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskDetailHandler.java
@@ -18,7 +18,16 @@ public class TaskDetailHandler implements Command {
     try (Connection con = DriverManager.getConnection( //
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
         PreparedStatement stmt = con.prepareStatement( //
-            "select * from pms_task where no=?")) {
+            "select "
+            + "   t.no,"
+            + "   t.content,"
+            + "   t.deadline,"
+            + "   t.status,"
+            + "   m.no as owner_no,"
+            + "   m.name as owner_name"
+            + " from pms_task t "
+            + "   inner join pms_member m on t.owner=m.no"
+            + " where t.no=?")) {
 
       stmt.setInt(1, no);
 
@@ -30,8 +39,8 @@ public class TaskDetailHandler implements Command {
 
         System.out.printf("내용: %s\n", rs.getString("content"));
         System.out.printf("마감일: %s\n", rs.getDate("deadline"));
-        System.out.printf("관리자: %s\n", rs.getString("owner"));
         System.out.printf("상태: %s\n", Task.getStatusLabel(rs.getInt("status")));
+        System.out.printf("관리자: %s\n", rs.getString("owner_name"));
       }
     }
   }
--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskUpdateHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/TaskUpdateHandler.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Task;
 import com.eomcs.util.Prompt;
 
@@ -28,6 +29,7 @@ public class TaskUpdateHandler implements Command {
                 + "    t.no,"
                 + "    t.content,"
                 + "    t.deadline,"
+                + "   t.status,"
                 + "    m.no as owner_no,"
                 + "    m.name as owner_name"
                 + "  from pms_task t"
@@ -37,7 +39,7 @@ public class TaskUpdateHandler implements Command {
             "update pms_task set"
                 + " content=?,"
                 + " deadline=?,"
-                + " owner=?"
+                + " owner=?,"
                 + " status=?"
                 + " where no=?")) { 
 
@@ -52,18 +54,25 @@ public class TaskUpdateHandler implements Command {
           System.out.println("해당 번호의 프로젝트가 없습니다.");
           return;
         }
-        task.setNo(no); 
+        task.setNo(no);
+        task.setContent(rs.getString("content"));
+        task.setDeadline(rs.getDate("deadline"));
+        Member owner = new Member();
+        owner.setNo(rs.getInt("owner_no"));
+        owner.setName(rs.getString("owner_name"));
+        task.setOwner(owner);
+        task.setStatus(rs.getInt("status"));
 
         // 2) 사용자에게서 변경할 데이터를 입력 받는다.
         task.setContent(Prompt.inputString(
             String.format("내용(%s)? ", rs.getString("content"))));
         task.setDeadline(Prompt.inputDate(
-            String.format("마감일(%s)? ", rs.getDate("sdt"))));
-        task.setOwner(memberValidator.inputMember(
-            String.format("만든이(%s)?(취소: 빈 문자열) ", rs.getString("owner_name"))));
+            String.format("마감일(%s)? ", rs.getDate("deadline"))));
         task.setStatus(Prompt.inputInt(String.format(
             "상태(%s)?\n0: 신규\n1: 진행중\n2: 완료\n> ", 
             Task.getStatusLabel(task.getStatus()))));
+        task.setOwner(memberValidator.inputMember(
+            String.format("만든이(%s)?(취소: 빈 문자열) ", rs.getString("owner_name"))));
 
 
         if (task.getOwner() == null) {
@@ -81,14 +90,13 @@ public class TaskUpdateHandler implements Command {
         stmt2.setString(1, task.getContent());
         stmt2.setDate(2, task.getDeadline());
         stmt2.setInt(3, task.getOwner().getNo());
-        stmt2.setDate(4, task.getDeadline());
-        stmt2.setInt(5, task.getStatus());
-        stmt2.setInt(6, task.getNo());
+        stmt2.setInt(4, task.getStatus());
+        stmt2.setInt(5, task.getNo());
         stmt2.executeUpdate();
 
         con.commit();
 
-        System.out.println("프로젝트을 변경하였습니다.");
+        System.out.println("작업 변경하였습니다.");
       }
     }
   }
