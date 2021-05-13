ace7220f6ca6b85005068c1c122acc93e01946fa
박선우 <sun960127@naver.com>
2021-05-13 16:09:33 +0900

    .

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectAdd3Handler.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/ProjectAdd3Handler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,18 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Project;
-import com.eomcs.pms.service.MemberService;
 import com.eomcs.pms.service.ProjectService;
 
 @SuppressWarnings("serial")
-@WebServlet("/project/add")
-public class ProjectAddHandler extends HttpServlet {
+@WebServlet("/project/add3")
+public class ProjectAdd3Handler extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
-    MemberService memberService = (MemberService) request.getServletContext().getAttribute("memberService");
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -42,16 +38,14 @@ public class ProjectAddHandler extends HttpServlet {
     out.println("시작일: <input type='date' name='startDate'><br>");
     out.println("종료일: <input type='date' name='endDate'><br>");
     out.println("팀원: <br>");
-    try {
-      List<Member> members = memberService.list(null);
-      for (Member m : members) {
-        out.printf("  <input type='checkbox' name='member' value='%d'>%s<br>\n", m.getNo(), m.getName());
-      }
-    } catch (Exception e) {
-      request.setAttribute("exception",e);
-      request.getRequestDispatcher("/error").forward(request, response);
-      return;
-    }
+
+    // 팀원 출력은 다른 서블릿에게 맡긴다.
+    request.getRequestDispatcher("/project/member/list").include(request, response);
+
+    // 인클루딩은 이전에 출력한 내용을 그대로 유지한다.
+    // 인클루딩 서블릿을 실행한 후, 리턴된 뒤에 수행하는 출력도 유효하다.
+    // 출력 내용을 
+
     out.println("<p><input type='submit' value='등록'>");
     out.println("<a href='list'>목록</a></p>");
 
