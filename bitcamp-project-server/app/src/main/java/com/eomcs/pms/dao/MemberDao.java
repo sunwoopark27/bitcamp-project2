f53983086a2b858d253c4a2676ea4a1af4577bce
sunwoopark27 <sun960127@naver.com>
2021-11-15 23:48:57 +0900

    .

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/dao/MemberDao.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/dao/MemberDao.java
@@ -1,6 +1,5 @@
 package com.eomcs.pms.dao;
 
-import java.util.List;
 import java.util.Map;
 import com.eomcs.pms.domain.Member;
 
@@ -8,7 +7,7 @@ public interface MemberDao {
 
   int insert(Member member) throws Exception;
 
-  List<Member> findAll() throws Exception;
+  Member findAll() throws Exception;
 
   Member findByNo(int no) throws Exception;
 
